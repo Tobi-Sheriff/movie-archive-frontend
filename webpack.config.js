7b8b8e1c0abdc4a34c417fb0a9ef06b20b8e697d
TobiSheriff <s.oluwatobiloba30@gmail.com>
2024-12-23 08:46:36 +0100

    check

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
 
 // const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
   mode: process.env.NODE_ENV || 'development', // Use 'development' mode by default
   devtool: 'source-map',
